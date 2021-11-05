b5671b60d82e6fb13af1e0336a62f1950298982c
Ans009 <86275168+Ans009@users.noreply.github.com>
2021-11-05 18:10:05 +0500

    initial commit

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
  }
 
  onSubmitSignIn = () => {
-    fetch('http://salute-face.herokuapp.com/signin', {
+    fetch('http://pure-inlet-71391.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
